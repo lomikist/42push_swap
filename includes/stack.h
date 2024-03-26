6c20e1d9c9ef2717524d0582371d0898c3061fa1
Arman <armansarg01@gmail.com>
2024-03-26 10:11:34 +0400

    dfd

--- includes/stack.h
+++ includes/stack.h
@@ -27,7 +27,7 @@ typedef struct s_engine
 	t_stack	stack_b;
 }	t_engine;
 
-int	ft_atoi(char *str);
+int		ft_atoi(char *str);
 t_node	*create_node(int index, int data);
-int	push(t_stack *stack, t_node *node);
+int		push(t_stack *stack, t_node *node);
 #endif
